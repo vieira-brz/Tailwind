bd415b80bc1620f46f244049519d683454968d6a
vieira-brz <vieirabraz2003@gmail.com>
2024-02-28 09:58:39 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Instalação
+```
+npx tailwindcss -o tailwind.css
+```
+
+# Arquivo de Configuração
+```
+npx tailwindcss init
+```
+
+# Arquivo Customizado
+```
+npx tailwindcss -i custom.css -o tailwind.css
+```
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          100: '#c542f5',
+          400: '#331040'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
